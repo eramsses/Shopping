ed40754d72a0b3d56125e4339e4e3d59ebc38ccd
Erick R. Rodríguez ASUS <47439411+eramsses@users.noreply.github.com>
2022-04-17 17:30:37 -0600

    Modificar perfil de usuario y cambio de contraseña

--- Shopping/Shopping/Helpers/IUserHelper.cs
+++ Shopping/Shopping/Helpers/IUserHelper.cs
@@ -8,6 +8,8 @@ namespace Shopping.Helpers
     {
         Task<User> GetUserAsync(string email);
 
+        Task<User> GetUserAsync(Guid userId);
+
         Task<IdentityResult> AddUserAsync(User user, string password);
 
         Task<User> AddUserAsync(AddUserViewModel model);
@@ -21,6 +23,12 @@ namespace Shopping.Helpers
         Task<SignInResult> LoginAsync(LoginViewModel model);
 
         Task LogoutAsync();
+
+        Task<IdentityResult> ChangePasswordAsync(User user, string oldPassword, string newPassword);
+
+        Task<IdentityResult> UpdateUserAsync(User user);
+
+
 
 
     }
